270d4344fda74cbe9f5c67e6b91ce0a8bfe16d31
arellanodavid <dtarellano1@gmail.com>
2016-06-29 20:45:45 -0700

    3rd push

---
+++ Day 2/app.js
@@ -0,0 +1,14 @@
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// console.log(firstName);
+// alert(lastName);
+// var fullName = confirm(firstName + " " + lastName);
+
+var yourColor = prompt("what is your favorite color?");
+var yourAge = prompt("How old are you?");
+var yourPet = prompt("What is the name of your Pet?");
+var yourHobbie = prompt("What is one of your hobbies?");
+
+console.log("Favorite Color: ", yourColor , "Your Age: ", yourAge , "Your Pet's Name: " , yourPet , "Your Hobbie: ", yourHobbie);
+
+document.write("Favorite Color: ", yourColor , "Your Age: ", yourAge , "Your Pet's Name: " , yourPet , "Your Hobbie: ", yourHobbie);
---
+++ Day 2/script.js
@@ -0,0 +1,5 @@
+//alert("This is a virus!");
+// var userResp = confirm("Will There be cake?");
+
+var userName = prompt("What is your name?");
+console.log(userName);
